b1bec7c329bcbcea71ceac18f56d83464cd5f720
Linda Rojas <lindadayana159@gmail.com>
2024-11-29 18:07:11 -0500

    modificacion de home

--- src/Home/InformationActivities/index.jsx
+++ src/Home/InformationActivities/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import './index.css'
 
 
 export function InformationActivities(
@@ -20,8 +19,8 @@ export function InformationActivities(
     
     
     return (
-        <div className='card flex bg-white shadow-slate-700 shadow-md'>
-            <figure className='img-card'>
+        <div className='w-full max-w-[291px] max-h-[84px] flex bg-white shadow-slate-700 shadow-md'>
+            <figure className='max-w-[97px] min-h-[84px]'>
                 <img 
                     className='object-cover w-full h-full'
                     src={imageCard} 
@@ -29,9 +28,9 @@ export function InformationActivities(
             </figure>
             <div className='flex flex-col'>
                     <span className='font-roboto font-medium text-lg text-center text-gray-600'>{title}</span>
-                    <p className='description font-roboto font-light text-description text-center'>{description}</p>
+                    <p className='font-roboto font-light text-[10px] text-center'>{description}</p>
                     <Link to={to}>
-                        <button className='color hover:bg-green-600 flex justify-center  mt-1 font-normal w-20 h-4 rounded-full ml-2 text-white text-xs '><span className='span'>{button}</span></button>
+                        <button className='bg-[#ef8f41e3] hover:bg-green-600 flex justify-center  mt-1 font-normal w-20 h-4 rounded-full ml-2 text-white text-xs '><span className='m-[-2px]'>{button}</span></button>
                     </Link>
 
             </div>
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,4 +1,3 @@
-import './index.css'
 import { InformationActivities } from './InformationActivities';
 import { CardsInformationHome } from '../mocks/cards-informations';
 
@@ -6,41 +5,40 @@ export function Home() {
     const LemaArmonia = `/assets/img/lema3.png`;
     const FondoPrincipal = `/assets/img/fondo.jpg`;
 
-
     return (
         <div className='flex flex-col items-center h-screen'>
             <div className='flex flex-col gap-2 items-center'>
                 <figure className='w-56 h-24'>
                     <img src={LemaArmonia} alt='Lema-armonia' />
                 </figure>
-                <section className='flex flex-col items-center gap-2'>
-                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-4xl'>EVENTOS</h1>
-                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-4xl'>DICIEMBRE</h1>
+                <section className='flex flex-col  items-center gap-2'>
+                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-[47px]'>EVENTOS</h1>
+                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-[47px]'>DICIEMBRE</h1>
                     <p className='font-roboto font-medium text-sm text-gray-500'>¿Qué evento te gustaría explorar?</p>
                 </section>    
             </div>
-            <div className='flex justify-items-center mt-[2rem]'>
-                <figure className='svg bg-cover bg-center h-64 sm:h-80 md:h-96 lg:h-128  bg-blue-700 opacity-85'>
+            <div className='flex justify-items-center mt-[2rem] relative'>
+                <figure className='svg w-full min-h-[40rem] left-0 z-[-1] rounded-tl-[70px] rounded-tr-[70px] bg-cover bg-center h-64 sm:h-80 md:h-96 lg:h-128 bg-blue-700 opacity-85'>
                     <img 
                     src={FondoPrincipal}
-                    className='svg-fondo w-full h-full opacity-30' 
+                    className='rounded-tl-[70px] rounded-tr-[70px] w-full h-full opacity-30' 
                     alt='Fondo-Principal' />
+                    {/* Aquí es donde vas a centrar los <InformationActivities /> */}
+                    <div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
+                        <div className='flex flex-wrap justify-center gap-8'>
+                            {CardsInformationHome.map(information => (
+                                <InformationActivities 
+                                    key={information.title}
+                                    imageName={information.imageName}
+                                    title={information.title}
+                                    description={information.description}
+                                    button={information.button}
+                                    to={information.to}/>
+                            ))}
+                        </div>
+                    </div>
                 </figure>
-                <div className='container-cards flex flex-wrap flex-col lg:flex-row gap-8 '>
-                
-                { CardsInformationHome.map( information => (
-                    <InformationActivities 
-                        key={information.title}
-                        imageName={information.imageName}
-                        title={information.title}
-                        description={information.description}
-                        button={information.button}
-                        to={information.to}/>
-                ))}
-            
-                </div>
-                
             </div>
         </div>
-    )
+    );
 }
