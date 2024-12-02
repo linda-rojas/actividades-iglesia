cc35f0f1880196ce8de7c2101b4d648359df81dd
Linda Rojas <lindadayana159@gmail.com>
2024-12-02 07:33:12 -0500

    corregir scroll

--- src/Home/InformationActivities/index.jsx
+++ src/Home/InformationActivities/index.jsx
@@ -19,8 +19,8 @@ export function InformationActivities(
     
     
     return (
-        <div className='w-full max-w-[291px] max-h-[84px] flex bg-white shadow-slate-700 shadow-md'>
-            <figure className='max-w-[97px] min-h-[84px]'>
+        <div className='w-full max-w-[325px] max-h-[100px] flex bg-white shadow-slate-700 shadow-md min-w-full rounded-bl-lg'>
+            <figure className='min-w-[97px] min-h-[84px]'>
                 <img 
                     className='object-cover w-full h-full'
                     src={imageCard} 
@@ -28,7 +28,7 @@ export function InformationActivities(
             </figure>
             <div className='flex flex-col'>
                     <span className='font-roboto font-medium text-lg text-center text-gray-600'>{title}</span>
-                    <p className='font-roboto font-light text-[10px] text-center'>{description}</p>
+                    <p className='font-roboto font-light text-[12px] text-center h-[43px]'>{description}</p>
                     <Link to={to}>
                         <button className='bg-[#ef8f41e3] hover:bg-green-600 flex justify-center  mt-1 font-normal w-20 h-4 rounded-full ml-2 text-white text-xs '><span className='m-[-2px]'>{button}</span></button>
                     </Link>
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -5,40 +5,42 @@ export function Home() {
     const LemaArmonia = `/assets/img/lema3.png`;
     const FondoPrincipal = `/assets/img/fondo.jpg`;
 
+
     return (
         <div className='flex flex-col items-center h-screen'>
-            <div className='flex flex-col gap-2 items-center'>
+            <div className='flex flex-col items-center'>
                 <figure className='w-56 h-24'>
                     <img src={LemaArmonia} alt='Lema-armonia' />
                 </figure>
-                <section className='flex flex-col  items-center gap-2'>
-                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-[47px]'>EVENTOS</h1>
-                    <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold text-[47px]'>DICIEMBRE</h1>
+                <section className='flex flex-col items-center gap-2'>
+                    <div className='text-lg'>
+                        <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold space-y-1'>EVENTOS</h1>
+                        <h1 className='font-montserrat flex-wrap text-center lg:block text-gray-700 font-extrabold space-y-1'>DICIEMBRE</h1>
+                    </div>
                     <p className='font-roboto font-medium text-sm text-gray-500'>¿Qué evento te gustaría explorar?</p>
                 </section>    
             </div>
-            <div className='flex justify-items-center mt-[2rem] relative'>
-                <figure className='svg w-full min-h-[40rem] left-0 z-[-1] rounded-tl-[70px] rounded-tr-[70px] bg-cover bg-center h-64 sm:h-80 md:h-96 lg:h-128 bg-blue-700 opacity-85'>
+            <div className='flex justify-items-center'>
+            <div className='relative top-[6rem] flex flex-wrap flex-col lg:flex-row gap-4 '>
+                
+                { CardsInformationHome.map( information => (
+                    <InformationActivities 
+                        key={information.title}
+                        imageName={information.imageName}
+                        title={information.title}
+                        description={information.description}
+                        button={information.button}
+                        to={information.to}/>
+                ))}
+                </div>
+                <figure className='svg fixed b-[0px] w-full min-h-[28rem] left-0 z-[-1] rounded-tl-[70px] rounded-tr-[70px] bg-cover bg-center bottom-[0] h-[16rem]  bg-blue-700 opacity-85'>
                     <img 
                     src={FondoPrincipal}
                     className='rounded-tl-[70px] rounded-tr-[70px] w-full h-full opacity-30' 
                     alt='Fondo-Principal' />
-                    {/* Aquí es donde vas a centrar los <InformationActivities /> */}
-                    <div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
-                        <div className='flex flex-wrap justify-center gap-8'>
-                            {CardsInformationHome.map(information => (
-                                <InformationActivities 
-                                    key={information.title}
-                                    imageName={information.imageName}
-                                    title={information.title}
-                                    description={information.description}
-                                    button={information.button}
-                                    to={information.to}/>
-                            ))}
-                        </div>
-                    </div>
                 </figure>
+                
             </div>
         </div>
-    );
+    )
 }
