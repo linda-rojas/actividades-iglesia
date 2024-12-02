0d02aa43f65c7b63af1643c1fd82639fd194f055
Linda Rojas <lindadayana159@gmail.com>
2024-12-02 14:10:14 -0500

    arreglar error de header

--- src/components/pages/ActivitiesCurch/index.jsx
+++ src/components/pages/ActivitiesCurch/index.jsx
@@ -10,7 +10,7 @@ export const ActivitiesCurch = () => {
                 <article className='flex ml-5'>
                     <ChevronLeftIcon className='w-5 h-[3rem] text-white' stroke='currentColor'/>
                 </article>
-                <h1 className='text-center font-roboto font-semibold text-[24px] text-white'>Iglesia la plata</h1>
+                <h1 className='text-center font-roboto font-semibold text-[24px] text-white select-none '>Iglesia la plata</h1>
             </header>
             <section className='relative top-[3rem] mb-[5rem] flex flex-wrap flex-col items-center lg:flex-row gap-8'>
             { CardsInformationCurch.map( information => (
