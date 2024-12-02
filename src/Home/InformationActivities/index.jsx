a265e1ca8da78cd0158a3fe5ea04907b970c1484
Linda Rojas <lindadayana159@gmail.com>
2024-12-02 16:14:13 -0500

    modificacion

--- src/Home/InformationActivities/index.jsx
+++ src/Home/InformationActivities/index.jsx
@@ -27,7 +27,7 @@ export function InformationActivities(
                     alt='image-card' />
             </figure>
             <div className='flex flex-col'>
-                    <span className='font-roboto font-medium text-[20px] text-center text-gray-600 w-[90%]'>{title}</span>
+                    <span className='font-roboto font-medium text-[20px] text-center self-center text-gray-600 w-[230px]'>{title}</span>
                     <p className='font-roboto font-light text-[14px] text-center h-[48px]'>{description}</p>
                     <Link to={to}>
                         <button className='bg-[#ef8f41e3] hover:bg-green-600 flex justify-center  mt-[0.75rem] font-normal w-20 h-4 rounded-full ml-2 text-white text-xs '><span className='m-[-1px]'>{button}</span></button>
