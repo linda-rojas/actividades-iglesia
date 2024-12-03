a5daaad18432273328d3100ae572ce9f0ec9ae28
Linda Rojas <lindadayana159@gmail.com>
2024-12-03 14:42:03 -0500

    Modificacion de font-size de ListToilet

--- src/components/pages/routes/ListToilet/index.jsx
+++ src/components/pages/routes/ListToilet/index.jsx
@@ -1,19 +1,24 @@
 import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { CardActivitiesToilet } from '../../../../mocks/cards-informations';
 import { ListInformation } from './ListInformation';
+import { Link } from 'react-router-dom';
+
 
 export const ListToilet = () => {
+    const ActivitiesCurch = '/ActivitiesCurch'
 
     return (
         <div>
-            <header className='h-[40px] bg-[#6aaaff] flex gap-[4rem] items-center w-full select-none'>
-                <article className='flex ml-5'>
-                    <ChevronLeftIcon className='w-5 h-4 text-white' stroke='currentColor'/>
-                </article>
-                <h1 className='text-center font-roboto font-semibold text-[17px] text-white'>Iglesia la plata</h1>
+            <header className='h-[50px] bg-[#3e8cf2] flex gap-[5rem] items-center w-full select-none'>
+                <Link to={ActivitiesCurch}>
+                    <article className='flex ml-5'>
+                        <ChevronLeftIcon className='w-5 h-[3rem] text-white' stroke='currentColor'/>
+                    </article>
+                </Link>
+                <h1 className='text-center font-roboto font-semibold text-[24px] text-white'>Listado de Aseo</h1>
             </header>
             <section className='relative top-[2rem] mb-[5rem] flex flex-wrap flex-col items-center lg:flex-row select-none'>
-            <div className='h-5 min-w-[270px] flex items-center justify-around text-[11px] text-white bg-green-700'>
+            <div className='h-5 min-w-[270px] flex items-center justify-around text-[13px] text-white bg-green-700'>
                 <span>Sábado</span>
                 <span>Familia</span>
                 <span>Celular</span>
